025ffb969b438ad7b85169f1b9dfb9cb95a07df0
liiws <gmlite@gmail.com>
2025-02-04 20:47:11 +0300

    [+] nuget

---
+++ nuget_publish.bat
@@ -0,0 +1,1 @@
+dotnet nuget push DocxGrider\bin\Release\DocxGrider.1.2.0.nupkg --api-key KEY --source https://api.nuget.org/v3/index.json
---
+++ nuget_publish_test.bat
@@ -0,0 +1,1 @@
+dotnet nuget push DocxGrider\bin\Release\DocxGrider.1.2.0.nupkg --api-key KEY --source https://apiint.nugettest.org/v3/index.json
