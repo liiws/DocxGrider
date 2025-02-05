16dadf3d2b4262f7b07db233ab039ed1b3d843b9
liiws <gmlite@gmail.com>
2025-02-05 18:55:26 +0300

    [+] nuget icon

--- nuget_publish.bat
+++ nuget_publish.bat
@@ -1,1 +1,1 @@
-dotnet nuget push DocxGrider\bin\Release\DocxGrider.1.2.0.nupkg --api-key KEY --source https://api.nuget.org/v3/index.json
+dotnet nuget push DocxGrider\bin\Release\DocxGrider.1.2.1.nupkg --api-key KEY --source https://api.nuget.org/v3/index.json
